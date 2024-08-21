9472bcc33024c3b96031e5c03761c67fc9475854
Jessica Shannon <me@jessshannon.com>
2024-08-21 17:02:47 +0100

    Add project files.

---
+++ FluentRepository/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+// DbContext
+using Microsoft.EntityFrameworkCore;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<LineItem> LineItems { get; set; }
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<Address> Addresses { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=:memory:");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+
+    public override void Dispose()
+    {
+        base.Dispose();
+    }
+}
---
+++ FluentRepository/Models.cs
@@ -0,0 +1,59 @@
+﻿
+public class Order
+{
+    public int Id { get; set; }
+    public DateTime OrderDate { get; set; }
+    public DateTime? ShippedDate { get; set; }
+    public OrderStatus Status { get; set; }
+    public Address? BillingAddress { get; set; }
+    public Address? DeliveryAddress { get; set; }
+    public Customer? Customer { get; set; }
+    public List<LineItem> LineItems { get; set; } = new();
+    public DateTime? PaymentConfirmedDate { get; set; }
+}
+
+public class LineItem
+{
+    public int Id { get; set; }
+    public string ProductName { get; set; }
+    public int Quantity { get; set; }
+    public decimal Price { get; set; }
+    public Order Order { get; set; }
+}
+
+public class Customer
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public Address? BillingAddress { get; set; }
+    public Address? DeliveryAddress { get; set; }
+}
+
+public class Address
+{
+    public int Id { get; set; }
+    public string Street { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+    public string PostalCode { get; set; }
+    public Country? Country { get; set; }
+}
+
+public class Country
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
+
+// Enum
+public enum OrderStatus
+{
+    Draft,
+    Submitted,
+    Shipped,
+    Delivered,
+    Returned,
+    Canceled
+}
